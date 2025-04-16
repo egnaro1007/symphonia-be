a908bcd43f54218e5759f464c7e863d2861debd8
egnaro1007 <ndviettuan@gmai.com>
2025-04-16 21:58:49 +0700

    Add: models for playlist and friendship

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+from library.models import Friendship         
+
+User.add_to_class(
+    'get_friends',
+    lambda self : [
+        friendship.user2 if friendship.user1 == self else friendship.user1
+        for friendship in Friendship.objects.filter(
+            models.Q(user1=self) | models.Q(user2=self),
+            status=Friendship.FriendshipStatus.ACCEPTED,
+        )
+    ] 
+)
+User.add_to_class(
+    'get_friend_requests',
+    lambda self : [
+        friendship.user2 if friendship.user1 == self else friendship.user1
+        for friendship in Friendship.objects.filter(
+            models.Q(user1=self) | models.Q(user2=self),
+            status=Friendship.FriendshipStatus.PENDING,
+        )
+    ]
+)
+User.add_to_class(
+    'get_friend_status',
+    lambda self, user: Friendship.objects.filter(
+        models.Q(user1=self, user2=user) | models.Q(user1=user, user2=self)
+    ).values_list('status', flat=True).first() or None
+)
+User.add_to_class(
+    'accept_friend_request',
+    lambda self, user: Friendship.objects.filter(
+        models.Q(user1=self, user2=user) | models.Q(user1=user, user2=self),
+        status=Friendship.FriendshipStatus.PENDING,
+    ).update(status=Friendship.FriendshipStatus.ACCEPTED)
+)
+User.add_to_class(
+    'reject_friend_request',
+    lambda self, user: Friendship.objects.filter(
+        models.Q(user1=self, user2=user) | models.Q(user1=user, user2=self),
+        status=Friendship.FriendshipStatus.PENDING,
+    ).delete()
+)
+User.add_to_class(
+    'send_friend_request',
+    lambda self, user: Friendship.objects.create(
+        user1=self,
+        user2=user,
+        status=Friendship.FriendshipStatus.PENDING,
+    )
+)
+User.add_to_class(
+    'remove_friend',
+    lambda self, user: Friendship.remove_friendship(self, user)
+)
--- library/models.py
+++ library/models.py
@@ -31,16 +31,6 @@ class Song(models.Model):
     def __str__(self):
         return f"{self.title} by {', '.join([artist.name for artist in self.artist.all()])}"
     
-class Playlist(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
-    songs = models.ManyToManyField(Song, related_name='playlists')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name
-    
 class ListeningHistory(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='listening_history')
     song = models.ForeignKey(Song, on_delete=models.CASCADE, related_name='listening_history')
@@ -52,3 +42,92 @@ class ListeningHistory(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.song.title} at {self.position}s"
+
+
+class SharingPermission(models.TextChoices):
+    PUBLIC = 'public', 'Public'
+    FRIENDS = 'friends', 'Friends'
+    PRIVATE = 'private', 'Private'
+
+class Friendship(models.Model):
+    class FriendshipStatus(models.TextChoices):
+        PENDING = 'pending', 'Pending'
+        ACCEPTED = 'accepted', 'Accepted'
+
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friendship_user1')
+    user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friendship_user2')
+    status = models.CharField(
+        max_length=10,
+        choices=FriendshipStatus.choices,
+        default=FriendshipStatus.PENDING,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('user1', 'user2')
+
+    def __str__(self):
+        return f"{self.user1.username} - {self.user2.username} - ({self.status})"
+    
+    def save(self, *args, **kwargs):
+        if self.user1 == self.user2:
+            raise ValueError("Users cannot be friends with themselves.")
+        if self.user1.id > self.user2.id:
+            self.user1, self.user2 = self.user2, self.user1
+
+        existing_friendship = Friendship.objects.filter(
+            user1=self.user1,
+            user2=self.user2,
+        ).first()
+
+        if existing_friendship is None:
+            super().save(*args, **kwargs)
+
+        if existing_friendship:
+            if existing_friendship.status == Friendship.FriendshipStatus.ACCEPTED:
+                return
+            elif existing_friendship.status == Friendship.FriendshipStatus.PENDING:
+                existing_friendship.status = Friendship.FriendshipStatus.ACCEPTED
+                existing_friendship.save()
+                self.status = Friendship.FriendshipStatus.ACCEPTED
+                return
+            
+    @classmethod
+    def remove_friendship(cls, user1, user2):
+        if user1 == user2:
+            raise ValueError("Users cannot be friends with themselves.")
+        friendship = Friendship.objects.filter(
+            models.Q(user1=user1, user2=user2) | models.Q(user1=user2, user2=user1)
+        ).first()
+        if friendship:
+            friendship.delete()
+            return True
+        return False
+
+
+class Playlist(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='playlists')
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    songs = models.ManyToManyField(Song, related_name='playlists')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    share_permission = models.CharField(
+        max_length=10,
+        choices=SharingPermission.choices,
+        default=SharingPermission.PRIVATE,
+    )
+
+    def __str__(self):
+        return self.name
+    
+    def is_accessible_by(self, user):
+        if self.owner == user:
+            return True
+
+        if self.share_permission == SharingPermission.PUBLIC:
+            return True
+        elif self.share_permission == SharingPermission.FRIENDS:
+            return self.owner.get_friend_status(user) == Friendship.FriendshipStatus.ACCEPTED
+        return False
