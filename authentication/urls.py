6a4a905dc37623fce914aa506c7aa4f1649ba6c9
egnaro1007 <ndviettuan@gmai.com>
2025-05-07 22:16:46 +0700

    Bug fixed: unfriend wrong behavior

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
-from authentication.views import RegisterUserAPIView, SearchUserAPIView, GetUserInfoAPIView, GetUserIDFromUsernameAPIView, FriendRequestAPIView, ResponseFriendRequestAPIView
+from authentication.views import RegisterUserAPIView, SearchUserAPIView, GetUserInfoAPIView, GetUserIDFromUsernameAPIView
+from authentication.views import FriendRequestAPIView, ResponseFriendRequestAPIView, UnfriendAPIView
 
 
 urlpatterns = [
@@ -11,11 +12,11 @@ urlpatterns = [
 
     path('register/', RegisterUserAPIView.as_view(), name='register_user'),
 
-    path('get_user_id/', GetUserIDFromUsernameAPIView.as_view(), name='get_user_id_from_username'),
-    path('friend_request/', FriendRequestAPIView.as_view(), name='friend_request'),
-    path('response_friend_request/', ResponseFriendRequestAPIView.as_view(), name='response_friend_request'),
-    path('unfriend/', ResponseFriendRequestAPIView.as_view(), name='unfriend'),
     path('search_user/', SearchUserAPIView.as_view(), name='search_user'),
     path('get_user_info/', GetUserInfoAPIView.as_view(), name='get_user_info'),
     path('get_user_info/<int:requested_user_id>/', GetUserInfoAPIView.as_view(), name='get_user_info'),
+    path('get_user_id/', GetUserIDFromUsernameAPIView.as_view(), name='get_user_id_from_username'),
+    path('friend_request/', FriendRequestAPIView.as_view(), name='friend_request'),
+    path('response_friend_request/', ResponseFriendRequestAPIView.as_view(), name='response_friend_request'),
+    path('unfriend/', UnfriendAPIView.as_view(), name='unfriend'),
 ]
